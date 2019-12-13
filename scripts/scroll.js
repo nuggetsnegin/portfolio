ecad7fdf66830ba79c7051ca600e0a5d2f2e4d0f
Negin <nuggetnegin@gmail.com>
2019-12-13 15:42:30 -0500

    added scroll reveal and lots of spans that I prob wont use :(

---
+++ scripts/scroll.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('.felineFeelingBlurb', { delay: 800 });
+ScrollReveal().reveal('.felineFeelingPreview', { delay: 1000 });
+
+
+ScrollReveal().reveal('.wishyWashyBlurb', { delay: 800 });
+ScrollReveal().reveal('.wishyWashyPreview', { delay: 1000 });
+
+
+ScrollReveal().reveal('.furiendFinderBlurb', { delay: 800 });
+ScrollReveal().reveal('.furiendFinderPreview', { delay: 1000 });
+
+ScrollReveal().reveal('.textileeBlurb', { delay: 800 });
+ScrollReveal().reveal('.textileePreview', { delay: 1000 });
