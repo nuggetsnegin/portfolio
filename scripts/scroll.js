9b4e963112661721339b49f1e95ab8bc98053339
Negin <nuggetnegin@gmail.com>
2019-12-15 16:43:20 -0500

    added a tag styling and fixed cascading on social media and text hyperlinks

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -2,14 +2,17 @@ ScrollReveal().reveal('.felineFeelingBlurb', { delay: 800 });
 ScrollReveal().reveal('.felineFeelingPreview', { delay: 1000 });
 
 
-ScrollReveal().reveal('.wishyWashyBlurb', { delay: 800 });
-ScrollReveal().reveal('.wishyWashyPreview', { delay: 1000 });
+ScrollReveal().reveal('.wishyWashyBlurb', { delay: 1200 });
+ScrollReveal().reveal('.wishyWashyPreview', { delay: 1500 });
 
 
-ScrollReveal().reveal('.furiendFinderBlurb', { delay: 800 });
-ScrollReveal().reveal('.furiendFinderPreview', { delay: 1000 });
+ScrollReveal().reveal('.furiendFinderBlurb', { delay: 1200 });
+ScrollReveal().reveal('.furiendFinderPreview', { delay: 1500 });
 
-ScrollReveal().reveal('.textileeBlurb', { delay: 800 });
-ScrollReveal().reveal('.textileePreview', { delay: 1000 });
+ScrollReveal().reveal('.textileeBlurb', { delay: 1200 });
+ScrollReveal().reveal('.textileePreview', { delay: 1500 });
+
+ScrollReveal().reveal('.gameOfPotterBlurb', { delay: 1200 });
+ScrollReveal().reveal('.gameOfPotterPreview', { delay: 1500 });
 
 ScrollReveal().reveal('.toolsContainer', { delay: 800 });
