c91b8d361346f3fc64682d01c6f3695692093a13
Negin <nuggetnegin@gmail.com>
2019-12-15 16:23:11 -0500

    added tools

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -11,3 +11,5 @@ ScrollReveal().reveal('.furiendFinderPreview', { delay: 1000 });
 
 ScrollReveal().reveal('.textileeBlurb', { delay: 800 });
 ScrollReveal().reveal('.textileePreview', { delay: 1000 });
+
+ScrollReveal().reveal('.toolsContainer', { delay: 800 });
